f2917498322573b172e1c70f73d8d91c2afc6a70
Pansaai <pans.thomas@gmail.com>
2018-06-08 15:36:35 +0200

    refactored fetching

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -6,10 +6,10 @@ class Pagination extends React.Component{
         return(
             <div className="content-container">
                 <div className="pagination">
-                    <button onClick={this.props.setUrlFirst}>First</button>
-                    <button onClick={this.props.setUrlPrev}>Prev</button>
+                    <button disabled={this.props.page < 2} onClick={this.props.first}>First</button>
+                    <button disabled={this.props.page < 2} onClick={this.props.setUrlPrev}>Prev</button>
                     <span>&#8226;</span>
-                    <button onClick={this.props.setUrlNext}>Next</button>
+                    <button onClick={this.props.next}>Next</button>
                     <button  onClick={this.props.setUrlLast}>Last</button>
                 </div>
             </div>
