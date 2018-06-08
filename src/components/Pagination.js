f40557c3fe5d8e0aec677dd1cae2abae894597cf
Pansaai <pans.thomas@gmail.com>
2018-06-08 16:29:21 +0200

    fixed prev

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -7,7 +7,7 @@ class Pagination extends React.Component{
             <div className="content-container">
                 <div className="pagination">
                     <button disabled={this.props.page < 2} onClick={this.props.first}>First</button>
-                    <button disabled={this.props.page < 2} onClick={this.props.setUrlPrev}>Prev</button>
+                    <button disabled={this.props.page < 2} onClick={this.props.prev}>Prev</button>
                     <span>&#8226;</span>
                     <button onClick={this.props.next}>Next</button>
                     <button  onClick={this.props.setUrlLast}>Last</button>
