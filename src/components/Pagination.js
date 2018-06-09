4b71ead0a45b9426345860d6ea291c22469a0572
Pansaai <pans.thomas@gmail.com>
2018-06-09 06:52:33 +0200

    added styles to pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -6,11 +6,11 @@ class Pagination extends React.Component{
         return(
             <div className="content-container">
                 <div className="pagination">
-                    <button disabled={this.props.page < 2} onClick={this.props.first}>First</button>
-                    <button disabled={this.props.page < 2} onClick={this.props.prev}>Prev</button>
+                    <button className="button" disabled={this.props.page < 2} onClick={this.props.first}>First</button>
+                    <button className="button" disabled={this.props.page < 2} onClick={this.props.prev}>Prev</button>
                     <span>&#8226;</span>
-                    <button onClick={this.props.next}>Next</button>
-                    <button  onClick={this.props.setUrlLast}>Last</button>
+                    <button className="button" onClick={this.props.next}>Next</button>
+                    <button className="button"  onClick={this.props.setUrlLast}>Last</button>
                 </div>
             </div>
         );
